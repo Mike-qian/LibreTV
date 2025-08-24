22fa2cd2a1c267a30a58a6cb8df135c53cb20373
Mike-qian <mike1994976451@outlook.com>
2025-08-24 16:19:37 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // 全局变量
-let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["tyyszy", "bfzy", "dyttzy", "ruyi", "ffzy", "zy360", "maotaizy", "wolong", "jisu", "dbzy", "mozhua", "mdzy", "zuid", "yinghua", "wujin", "wwzy", "lzi", "qgbaiduyun", "qgwujin", "qgsuoni", "qghongnui", "qgfeifan", "qglingzi", "qgqungsu", "qgck", "qgshandian", "qghuohu", "qghuya", "qgsiquan", "qgxinlang", "qgjinying", "qgyouzhiziyuanku", "qgtaopian", "qguku", "qgikun"]'); // 默认选中资源
+let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["tyyszy", "bfzy", "dyttzy", "ruyi", "ffzy", "zy360", "maotaizy", "wolong", "jisu", "dbzy", "mozhua", "mdzy", "zuid", "yinghua", "wujin", "wwzy", "lzi", "qgbaiduyun", "qgwujin", "qgsuoni", "qghongnui", "qgfeifan", "qglingzi", "qgck", "qgshandian", "qghuohu", "qgsiquan", "qgxinlang", "qgtaopian", "qgikun"]'); // 默认选中资源
 let customAPIs = JSON.parse(localStorage.getItem('customAPIs') || '[]'); // 存储自定义API列表
 
 // 添加当前播放的集数索引
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
         // 默认选中资源
-        selectedAPIs = ["tyyszy", "bfzy", "dyttzy", "ruyi", "ffzy", "zy360", "maotaizy", "wolong", "jisu", "dbzy", "mozhua", "mdzy", "zuid", "yinghua", "wujin", "wwzy", "lzi", "qgbaiduyun", "qgwujin", "qgsuoni", "qghongnui", "qgfeifan", "qglingzi", "qgqungsu", "qgck", "qgshandian", "qghuohu", "qghuya", "qgsiquan", "qgxinlang", "qgjinying", "qgyouzhiziyuanku", "qgtaopian", "qguku", "qgikun"];
+        selectedAPIs = ["tyyszy", "bfzy", "dyttzy", "ruyi", "ffzy", "zy360", "maotaizy", "wolong", "jisu", "dbzy", "mozhua", "mdzy", "zuid", "yinghua", "wujin", "wwzy", "lzi", "qgbaiduyun", "qgwujin", "qgsuoni", "qghongnui", "qgfeifan", "qglingzi", "qgck", "qgshandian", "qghuohu", "qgsiquan", "qgxinlang", "qgtaopian", "qgikun"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
 
         // 默认选中过滤开关
